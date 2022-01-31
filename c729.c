14da925d767f2834e6a407b3714a14d6742b0b10
haoenting <91521504+haoenting@users.noreply.github.com>
2022-01-31 17:02:31 +0800

    Create c729.c

---
+++ c729.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main(){
+    char name1[100], name2[100];
+    scanf("%s %s", name1, name2);
+    printf("%s and %s sitting in the tree", name1, name2);
+    return 0;
+}
