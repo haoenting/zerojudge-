6ef87cbe4278e519979f365b612482af36b4fa8b
丁晧恩 <tinghaoen@tinghaoen.local>
2024-07-04 21:10:25 +0800

    upload d229

--- d/d229.cpp
+++ d/d229.cpp
@@ -1,22 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int F=1, O=0, R=0,T=1, Y=0, E=0, N=0, S=1, I=0, X=0;
-    // F*1000+O*1000+R*100+T*210+Y+ E * 20 + N*2 
-    // = S*1000+I*1000+X*100+ T*10 + Y
+    int F=1, T=1, Y=0, E=5, N=0;
     while(F<10){
+        int O = 0;
         while(O<10){
+            if(F == O+1)
+                continue;
+            int R = 0;
+            while(R<10){
 
-                while(Y<10){
-                    if(Y == N)
+                int T = 1;
+                while(T < 10){
+                    if(R == T || R + T+T <9)
                         continue;
 
-                    Y++;
-                }   
+                    int Y =0;
+                    while(Y < 10){
+                        if(T == Y)
+                            continue;
+
+                        int SIX = F*100+O*10+R+1;
+                        if(R+T+T>9 && SIX <1000){
+                            cout<<F<<O<<R<<T<<Y;
+                            cout<<" + ";
+                            cout<<T<<E<<N;
+                            cout<<" = ";
+                            cout<<SIX<<T<<Y<<endl;
+                        }
+                        E = 5;
+
+                        
+                    }
+                    T++;
+                }
+                R++;
+            }
             O++;
         }
         F++;
     }
-
+    //R+2T >= 10
     return 0;
 }
